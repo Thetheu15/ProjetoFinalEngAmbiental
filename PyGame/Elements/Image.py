9fd077e15cb9073d7f9353812bb4d969e00b220e
GabrrielGSB <g.gonzaga.barreto@gmail.com>
2025-02-03 15:53:53 -0300

    ...

--- PyGame/Elements/Image.py
+++ PyGame/Elements/Image.py
@@ -3,8 +3,8 @@ import pygame
 class Image:
     def __init__(self, image_path, position):
         self.image_path = image_path
-        self.image = pygame.image.load(image_path).convert()
-        self.rect = self.image.get_rect(topleft=position)
+        self.image      = pygame.image.load(image_path).convert()
+        self.rect       = self.image.get_rect(topleft=position)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
